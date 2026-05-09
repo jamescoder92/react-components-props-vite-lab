5f0dbfc74b4a606a7db9510b48c8be332a7d82bd
jamescoder92 <kusimbajames92@gmail.com>
2026-05-09 17:29:17 +0300

    Articlelist

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,16 @@
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
